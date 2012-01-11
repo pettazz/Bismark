b7b9fade0b0dc580ad7b8ce63ef63135d13ce5de
Nick Pettazzoni <pettazz@gmail.com>
2012-01-10 21:13:08 -0500

    oh come on

---
+++ jacked_configbismark.php
@@ -0,0 +1,16 @@
+<?php
+
+	//JACKED Manager module configuration file
+	////JACKED Bismark configuration settings
+	
+	//Bismarks
+	define(JACKED_BISMARK_DB_MARKS, 'Bismark');
+	define(JACKED_BISMARK_DB_REPLIES, 'Reply');
+	define(JACKED_BISMARK_DB_USERS, 'Person');
+	define(JACKED_BISMARK_DB_MARKRELS, 'BismarkCompleteRel');
+	define(JACKED_BISMARK_DB_FRIENDS, 'FriendRel');
+	define(JACKED_BISMARK_DB_CHAINS, 'Chain');
+	define(JACKED_BISMARK_DB_CHAINRELS, 'ChainRel');
+	define(JACKED_BISMARK_DB_CHAINSDONE, 'ChainCompleteRel');
+
+?>
